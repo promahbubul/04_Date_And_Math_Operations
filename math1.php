99c03eb21069f424f805dc69ff4da773a6ea069f
promahbubul <mahbublalam500@gmail.com>
2025-06-16 14:12:54 +0600

    Math Operators: ceil(), floor(), round() method.

---
+++ math1.php
@@ -0,0 +1,12 @@
+<?php
+
+$pieces = 4;
+$guest = 18;
+
+$numberOfPizzas = ceil($guest / $pieces);
+// $numberOfPizzas = floor($guest / $pieces);
+
+
+// echo $numberOfPizzas;
+// echo round(10.5); "\n";
+echo round(10.49);
